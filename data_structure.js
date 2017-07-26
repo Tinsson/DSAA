f60d8651502eb8c552ae02e12fe152983c2dfa0f
Tinsson <695724046@qq.com>
2017-07-26 14:00:23 +0800

    data

--- data_structure.js
+++ data_structure.js
@@ -101,3 +101,62 @@ function Set(){
 	}
 }
 
+//字典和集合很相似，也称作映射，可以用Map类来实现
+function Dictionary(){
+	var items = {};
+	this.has = function(key){
+		return key in items;
+	}
+
+	this.set = function(key, value){
+		items[key] = value;
+	}
+
+	this.remove = function(key){
+		if(this.has(key)){
+			delete items[key];
+			return true;
+		}
+		return false;
+	}
+
+	this.get = function(key){
+		return this.has(key) ? items[key] : undefined;
+	}
+
+	this.values = function(){
+		var values = {};
+		for(var k in items){
+			if(this.has(k)){
+				values.push(items[k]);
+			}
+		}
+		return values;
+	}
+}
+
+//使用散列算法可以知道值的具体位置
+var loseloseHashCode = function(key){
+	var hash = 0;
+	for(var i = 0; i < key.length;i++){
+		hash += key.charCodeAt(i);
+	}
+	return hash % 37;
+}
+function HashTable(){
+	var table = [];
+	
+	this.put = function(key,value){
+		var position = loseloseHashCode(key);
+		console.log(position + ' - '+ key);
+		table[position] = value;
+	}
+	
+	this.get = function(key){
+		return table[loseloseHashCode(key)];
+	}
+
+	this.remove = function(key){
+		table[loseloseHashCode(key)] = undefined;
+	}
+}
