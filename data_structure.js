21d1f0dbf06db936fa6c733c3b5b638ed6f383d3
Tinsson <695724046@qq.com>
2017-07-27 11:03:11 +0800

    graph

--- data_structure.js
+++ data_structure.js
@@ -447,3 +447,41 @@ var removeNode = function(node,key){
 		return node;
 	}
 }
+var findMinNode = function(node){
+	if(node){
+		while(node && node.left !== null){
+			node = node.left;
+		}
+		return node;
+	}
+	return null;
+}
+
+//图类
+function Graph(){
+	var vertices = [];
+	var adjList = new Dictionary();
+
+	this.addVertex = function(v){
+		vertices.push(v);
+		adjList.set(v, []);
+	}
+
+	this.addEage = function(v, w){
+		adjList.get(v).push(w);
+		adjList.get(w).push(v);
+	}
+
+	this.toString = function(){
+		var s = '';
+		for(var i = 0; i<vertices.length;i++){
+			s += vertices[i] + ' -> ';
+			var neighbors = adjList.get(vertices[i]);
+			for(var j = 0;j < neighbors.length; j++){
+				s += neighbors[j] + ' ';
+			}
+			s += '\n';
+		}
+		return s;
+	}
+}
