c46a6f49f28372fd3c501f1dcf9c9ffab9fbea12
Tinsson <695724046@qq.com>
2017-07-29 17:21:15 +0800

    dsa

--- algorithm.js
+++ algorithm.js
@@ -6,10 +6,38 @@ function ArrayList(){
 	this.toString = function(){
 		return array.join();
 	};
+	//数值交换辅助函数
+	var swap = function(index1,index2){
+		var aux = array[index1];
+		array[index1] = array[index2];
+		array[index2] = aux;
+	}
 	//冒泡排序
 	this.bubbleSort = function(){
 		var length = array.length;
-		for()
+		for(var i = 0; i< length;i++){
+			for(var j = 0;j< length - i -1 ;j++){
+				if(array[j] > array[j+1]){
+					swap(j, j+1);
+				}
+			}
+		}
+	}
+	//选择排序
+	this.selectionSort = function(){
+		var length = array.length,
+			indexMin;
+		for(var i = 0;i < length -1;i++){
+			indexMin = i;
+			for(var j = i;j<length;j++){
+				if(array[indexMin] > array[j]){
+					indexMin = j;
+				}
+			}
+			if(i !== indexMin){
+				swap(i, indexMin);
+			}
+		}
 	}
 }
-//冒泡私有函数
+
