46fb56166be157b7efc20a53a706cd99a51b93a4
Tinsson <695724046@qq.com>
2017-07-29 08:27:04 +0800

    sort

---
+++ algorithm.js
@@ -0,0 +1,15 @@
+function ArrayList(){
+	var array = [];
+	this.insert = function(item){
+		array.push(item);
+	};
+	this.toString = function(){
+		return array.join();
+	};
+	//冒泡排序
+	this.bubbleSort = function(){
+		var length = array.length;
+		for()
+	}
+}
+//冒泡私有函数
