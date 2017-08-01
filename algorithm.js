0b58474e22595ab97765b735dbf120b49c101409
Tinsson <695724046@qq.com>
2017-08-01 15:55:47 +0800

    quicksort

--- algorithm.js
+++ algorithm.js
@@ -12,6 +12,73 @@ function ArrayList(){
 		array[index1] = array[index2];
 		array[index2] = aux;
 	}
+	//归并排序辅助函数
+	var mergeSortRec = function(array){
+		var length = array.length;
+		if(length === 1){
+			return array;
+		}
+		var mid = Math.floor(length / 2),
+			left = array.slice(0,mid),
+			right = array.slice(mid,length);
+		return merge(mergeSortRec(left),mergeSortRec(right));
+	}
+	var merge = function(left,right){
+		var result = [],
+			il = 0,
+			ir = 0;
+		while(il < left.length && ir < right.length){
+			if(left[il] < right[ir]){
+				result.push(left[il++]);
+			}else{
+				result.push(right[ir++]);
+			}
+		}
+		while(il < left.length){
+			result.push(left[il++]);
+		}
+		while(ir < right.length){
+			result.push(right[ir++]);
+		}
+		return result;
+	}
+	//快速排序辅助函数
+	var quick = function(array, left, right){
+		var index;
+		if(array.length > 1){
+			index = partition(array,left,right);
+			if(left < index-1){
+				quick(array,left,index -1);
+			}
+			if(index < right){
+				quick(array,index,right)
+			}
+		}
+	}
+	var partition = function(array, left, right){
+		var pivot = array[Math.floor((right+left) / 2)],
+			i = left,
+			j = right;
+		while(i <= j){
+			while(array[i] < pivot){
+				i++;
+			}
+			while(array[j] > pivot){
+				j++;
+			}
+			if(i <= j){
+				swapQuickStort(array, i, j);
+				i++;
+				j--;
+			}
+		}
+		return i;
+	}
+	var swap = function(array,index1,index2){
+		var aux = array[index1];
+		array[index1] = array[index2];
+		array[index2] = aux;
+	}
 	//冒泡排序
 	this.bubbleSort = function(){
 		var length = array.length;
@@ -38,6 +105,28 @@ function ArrayList(){
 				swap(i, indexMin);
 			}
 		}
+	}
+	//插入排序
+	this.insertionSort = function(){
+		var length = array.length,
+			j, temp;
+		for(var i=1; i<length; i++){
+			j = i;
+			temp = array[i];
+			while(j>0 && array[j-1] > temp){
+				array[j] = array[j-1];
+				j--;
+			}
+			array[j] = temp;
+		}
+	}
+	//归并排序
+	this.mergeSort = function(){
+		array = mergeSortRec(array);
+	}
+	//快速排序
+	this.quickSort = function(){
+		quick(array, 0, array.length - 1);
 	}
 }
 
